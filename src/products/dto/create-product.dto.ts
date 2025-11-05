b88022b7f17756b908e6baf0f922fddaab701646
ncastellanosort <nicolasfelipe00234@gmail.com>
2025-11-04 23:29:25 -0500

    chore: new product type

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,11 +1,24 @@
 export type CreateProductDto = {
   sku: string;
+  barcode?: string;
   name: string;
+  brand: string;
+  tags: string[];
   description?: string | null;
+  category_id?: number | null;
+  supplier_id?: number | null;
+
   purchase_price: number;
   sale_price: number;
   tax: number;
+  currency: string;
+
   stock: number;
   min_stock: number;
   unit: string;
+  weight?: number | null;
+
+  status: 'active' | 'inactive' | 'discontinued';
+
+  image_urls?: string[];
 };
--- src/products/types/product.type.ts
+++ src/products/types/product.type.ts
@@ -2,17 +2,29 @@ export type Product = {
   id: number;
   company_id: number;
   sku: string;
+  barcode?: string;
   name: string;
+  brand: string;
+  tags: string[];
   description?: string | null;
   category_id?: number | null;
   supplier_id?: number | null;
+
   purchase_price: number;
   sale_price: number;
   tax: number;
+  currency: string;
+
   stock: number;
   min_stock: number;
   unit: string;
-  is_active: boolean;
+  weight?: number | null;
+
+  status: 'active' | 'inactive' | 'discontinued';
+
+  image_urls?: string[];
+
   created_at: string;
   updated_at: string;
+  deleted_at?: string | null;
 };
